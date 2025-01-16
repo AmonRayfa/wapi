ec9be659b719bc75fea184b8877795d611232057
AmonRayfa <amon.rayfa@proton.me>
2025-01-16 01:31:07 +0100

    [minor] Created a custom `Error` and `Result` type for the `api` module and added a variant named `Cache` to handle the errors of the `Cache` struct methods.

--- src/error/api.rs
+++ src/error/api.rs
@@ -1,2 +1,18 @@
 // Copyright 2025 Amon Rayfa.
 // SPDX-License-Identifier: Apache-2.0.
+
+//! This module contains the custom `Error` and `Result` types for the `api` module.
+
+use mabe::Mabe;
+
+/// The custom `Error` type for the `api` module.
+#[derive(Mabe)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("Cache manipulation failed: enable to {0} the cache.")]
+    #[debug("{1}")]
+    Cache(String, String),
+}
+
+/// The custom `Result` type for the `api` module.
+pub type Result<T> = std::result::Result<T, Error>;
